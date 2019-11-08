68c345ad0ac755792baa98a9fa2c821107efd38a
Matt DeMonaco <mdemonaco@gmail.com>
2019-11-07 21:26:25 -0800

    added container for train information to be stored in

---
+++ app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+$("button").on("click", function(){
+    event.preventDefault;
+var train1input = $("#formGroupExampleInput").val().trim();
+console.log("hello");
+$("#train1").text(train1input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+});
