f946332d95162adb176c3eedd811478fab39552a
Matt DeMonaco <mdemonaco@gmail.com>
2019-11-09 19:51:53 -0800

    Train schedule form UI completed

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCjt08ZdWW2AGyhuUASckAliJt_7EeERZs",
+    authDomain: "train-scheduler-6fcad.firebaseapp.com",
+    databaseURL: "https://train-scheduler-6fcad.firebaseio.com",
+    projectId: "train-scheduler-6fcad",
+    storageBucket: "train-scheduler-6fcad.appspot.com",
+    messagingSenderId: "46413369280",
+    appId: "1:46413369280:web:49d16de603dcd163608225",
+    measurementId: "G-Z6QS05HLD6"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 $(document).ready(function(){
 $("button").on("click", function(){
     event.preventDefault();
